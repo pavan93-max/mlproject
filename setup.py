99d331005b2b801edeb37cd846b0125f804e5430
pavan93-max <pavanraut060@gmail.com>
2025-06-01 13:06:56 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+from typing import List
+
+def get_requirements(file_path:str) -> List[str]:
+    '''
+    This function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
+        if '-e .' in requirements:
+             requirements.remove('-e .')
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='pavan',
+    author_email='pavanraut060@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+)
